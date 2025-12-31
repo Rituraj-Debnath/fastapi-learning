baa92200726d3e88e84c600cf0f4f3921d368f3f
Rituraj-Debnath <riturajdebnathfit@gmail.com>
2025-12-31 12:41:37 +0530

    HTTPexception added

--- project 1/main.py
+++ project 1/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI  
+from fastapi import FastAPI , Path , HTTPException
 import json
-from fastapi import Path
+
 # Path is a helper function used to provide metadata, description, data validation for our path parameters so user can understand before giving input
+#Httpexception is a build it exception class in fastapi to provide custom http error response in our api.
 
 def p_data():
     with open("patient_data.json","r") as odin: #odin is just a var
@@ -26,4 +27,4 @@ def only(patient_id: str = Path(..., description="write patient id here", exampl
     data = p_data()
     if patient_id in data:
         return data[patient_id]
-    return {"error": "patient not found"}
+    raise HTTPException(status_code=404 , detail="The patient is not present in our database")
