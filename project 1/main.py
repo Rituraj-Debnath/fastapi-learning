05bfb4ee5d40affbd2fff0473a315cb87ba24cf9
Rituraj-Debnath <riturajdebnathfit@gmail.com>
2025-12-31 09:34:44 +0530

    path params hands on

--- project 1/main.py
+++ project 1/main.py
@@ -1,6 +1,7 @@
-from fastapi  import FastAPI 
+from fastapi import FastAPI  
 import json
-
+from fastapi import Path
+# Path is a helper function used to provide metadata, description, data validation for our path parameters so user can understand before giving input
 
 def p_data():
     with open("patient_data.json","r") as odin: #odin is just a var
@@ -8,11 +9,21 @@ def p_data():
         return hella  #hella is also a var
 
 
-thor = FastAPI()   #thor is obj
-@thor.get("/loki")
+app = FastAPI()   #app is obj
+@app.get("/loki")
 
 def asguard():
    stark = p_data() #start is also an var
    return stark
 
 # all the var are storing values 
+
+
+#### adding the concept of query/path parameters
+
+@app.get("/only/{patient_id}")
+def only(patient_id: str = Path(..., description="write patient id here", example="001,002" , min_length=3)):
+    data = p_data()
+    if patient_id in data:
+        return data[patient_id]
+    return {"error": "patient not found"}
